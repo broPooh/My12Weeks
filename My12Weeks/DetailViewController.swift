1c9af2449dd47cb5dbc3fa313e2de28ec7ca614f
broMac <yhs4705@naver.com>
2022-05-30 12:28:10 +0900

    코드로 화면 작성 테스트

--- My12Weeks/DetailViewController.swift
+++ My12Weeks/DetailViewController.swift
@@ -9,16 +9,98 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    let bannerView = BannerView()
+    let titleLabel = UILabel()
+    let captionLabel = UILabel()
+    let activateButton = MainActivateButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .lightGray
         
-        bannerView.frame = CGRect(x: 30, y: 100, width: UIScreen.main.bounds.width - 60, height: 120)
-        view.addSubview(bannerView)
+        
+        view.backgroundColor = .white
+        
+        setTitleLabelConstraints()
+        setCaptionLabelConstraints()
+        setActicateButtonConstraints()
+        
+    }
+    
+    func setActicateButtonConstraints() {
+        view.addSubview(activateButton)
+        activateButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activateButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activateButton.widthAnchor.constraint(equalToConstant: 300),
+            activateButton.heightAnchor.constraint(equalToConstant: 100),
+            activateButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        
+    }
+    
+    
+    func setTitleLabelConstraints() {
+        titleLabel.text = "관심있는 회사\n3개를 선택해주세요"
+        titleLabel.backgroundColor = .lightGray
+        titleLabel.font = .boldSystemFont(ofSize: 24)
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 0
+        
+        
+        //NSLayoutConstraints 사용 주의점
+        //NSLayoutConstraints를 지정하기 전에 먼저 뷰를 등록을 해주어야 한다.
+        //등록을 먼저 해주지 않게 된다면 속성을 지정하려고 할때 에러가 발생한다.(없기 때문)
+        view.addSubview(titleLabel)
+        
+        //오토레이아웃의 속성을 덮어쓸 수 있도록 해주기 위해서 false값으로 변경을 해주어야 한다.
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        //autoLayout 이전의 코드
+        //titleLabel.frame = CGRect(x: 100, y: 100, width: UIScreen.main.bounds.width - 200, height: 80)
+        
+        //NSLayoutConstraints
+        let topConstraint = NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 0)
+        topConstraint.isActive = true
+        
+        NSLayoutConstraint(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 40).isActive = true
+        NSLayoutConstraint(item: titleLabel, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: -40).isActive = true
+        NSLayoutConstraint(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 80).isActive = true
+    }
+    
+    func setCaptionLabelConstraints() {
+        captionLabel.text = "맞춤 정보를 알려드려요"
+        captionLabel.backgroundColor = .lightGray
+        captionLabel.font = .boldSystemFont(ofSize: 18)
+        captionLabel.textAlignment = .center
+        captionLabel.numberOfLines = 0
+        
+        
+        //NSLayoutConstraints 사용 주의점
+        //NSLayoutConstraints를 지정하기 전에 먼저 뷰를 등록을 해주어야 한다.
+        //등록을 먼저 해주지 않게 된다면 속성을 지정하려고 할때 에러가 발생한다.(없기 때문)
+        view.addSubview(captionLabel)
+        
+        //오토레이아웃의 속성을 덮어쓸 수 있도록 해주기 위해서 false값으로 변경을 해주어야 한다.
+        captionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        //autoLayout 이전의 코드
+        //titleLabel.frame = CGRect(x: 100, y: 100, width: UIScreen.main.bounds.width - 200, height: 80)
+        
+        //NSLayoutConstraints
+        let topConstraint = NSLayoutConstraint(item: captionLabel, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 10)
+        
+        //루트 뷰의 가운데에 배치하고 싶을때?
+        let centerX = NSLayoutConstraint(item: captionLabel, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
+    
+        let width = NSLayoutConstraint(item: captionLabel, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 0.7, constant: 0)
+        
+        let height = NSLayoutConstraint(item: captionLabel, attribute: .height, relatedBy: .equal, toItem: titleLabel, attribute: .height, multiplier: 0.5, constant: 0)
+        
+        view.addConstraints([topConstraint, centerX, width, height])
 
+        
     }
     
 
--- My12Weeks/MainActivateButton.swift
+++ My12Weeks/MainActivateButton.swift
@@ -25,4 +25,16 @@ class MainActivateButton: UIButton {
         set { layer.borderColor = newValue.cgColor }
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .systemBlue
+        self.setTitleColor(.white, for: .normal)
+        self.cornerRadius = 10
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
